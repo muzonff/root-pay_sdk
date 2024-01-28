9869d0b567212b440ba38edbd80583a65576acff
muzonff <51887793+muzonff@users.noreply.github.com>
2024-01-28 23:38:35 +0300

    Update setup.py
    add PyPi readme

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf8) as f:
+    readme = f.read()
+    
 requirements = ["httpx>=0.26.0"]
 print(find_packages())
 setup(
     name="rootpay_sdk",
-    version="0.0.3",
+    version="0.0.4",
     author="Touka",
     author_email="touka.touka@icloud.com",
     description="A package to interact with root-pay.app API",
-    long_description="A package to interact with root-pay (update later)",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/muzonff/root-pay_sdk/",
     packages=find_packages(),
     install_requires=requirements,
